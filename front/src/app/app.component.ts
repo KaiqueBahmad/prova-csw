02ac2681f07fd71ef5d8a90dc846fb7350ac8a9f
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-05-27 19:33:24 -0300

    Ajustes setup front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -4,8 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <p>ola mundo</p>
+    <router-outlet></router-outlet>
+  `
 })
 export class AppComponent {
   title = 'biblioteca';
