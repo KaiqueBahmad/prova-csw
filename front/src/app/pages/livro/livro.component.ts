a7ee39a78da4489815f27034f87f0512da9d2e0a
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-05-27 20:47:29 -0300

    adicionado toast

--- front/src/app/pages/livro/livro.component.ts
+++ front/src/app/pages/livro/livro.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
 import { DatePicker } from 'primeng/datepicker';
 import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-livro',
-  imports: [ReactiveFormsModule, DatePicker, InputTextModule, ButtonModule],
+  imports: [ReactiveFormsModule, DatePicker, InputTextModule, ButtonModule, ToastModule],
   template: `
+    <p-toast></p-toast>
     <div style="width: 100%; display: flex;">
       <div style="flex: 1; display: flex; align-items: center; flex-direction: column; gap: 25px;">
         <h1>Cadastre um novo livro 📖 </h1>  
@@ -30,7 +33,7 @@ import { InputTextModule } from 'primeng/inputtext';
             </p-date-picker>
         </div>
 
-        <p-button>
+        <p-button (onClick)="cadastrarLivro()">
           Salvar
         </p-button>
       </div>
@@ -43,5 +46,18 @@ import { InputTextModule } from 'primeng/inputtext';
   `
 })
 export class LivroComponent {
+  constructor(private messageService: MessageService) {
+
+  }
+
+  cadastrarLivro() {
+    this.messageService.add({
+      severity: 'success',
+      summary: 'Salvo',
+      detail: 'Novo Livro foi registrado com sucesso!',
+      life: 3000
+    });
+  }
+
 
 }
