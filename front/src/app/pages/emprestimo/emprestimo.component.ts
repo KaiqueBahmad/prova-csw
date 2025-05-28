543567752d4c116773d8860a768221557e73ec58
Kaique Bahmad <kaiquebahmadt@gmail.com>
2025-05-27 22:35:37 -0300

    inicio emprestimo

--- front/src/app/pages/emprestimo/emprestimo.component.ts
+++ front/src/app/pages/emprestimo/emprestimo.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-emprestimo',
   imports: [],
-  templateUrl: './emprestimo.component.html',
-  styleUrl: './emprestimo.component.css'
+  template: `
+    <div style="width: 100%; display: flex;">
+      <div style="flex: 1; display: flex; align-items: center; flex-direction: column; gap: 25px;">
+        
+      </div>
+    </div>
+  `
 })
 export class EmprestimoComponent {
 
